be94650ac4e61e1d91d3bc07ffa1272e16437ce6
Roey Darwish Dror <roeyd@infinidat.com>
2015-04-08 16:41:54 +0300

    Fix a few things with deb packing

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 pack:
+	mkdir -p target/deb
 	docker build -t transporter .
-	docker run -v $(PWD):/src --rm -ti transporter
+	docker run -v $(PWD):/src -v $(PWD)/target/deb:/src/target --rm -ti transporter
 
 clean:
 	rm -rf target/release_dir
---
+++ deb/pack.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+cd /src
+cargo update
+cargo build --release
+
+rm -rf /pack
+mkdir -p /pack/etc
+mkdir -p /pack/usr/bin
+cp /src/target/release/transporter /pack/usr/bin/transporter
+cp /src/example/transporter.conf /pack/etc
+fpm -s dir -t deb -v `grep version Cargo.toml | awk '{print $3}' | sed 's/"//g'` -n transporter -C /pack --config-files etc/transporter.conf -f  .
